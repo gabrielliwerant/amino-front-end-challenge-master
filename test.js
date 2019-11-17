97818e8193175fcb9f18f1a3f83b58398df16fe2
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-11-17 18:27:32 -0500

    Add test and update tests to accommodate office argument

--- test.js
+++ test.js
@@ -12,7 +12,7 @@ var stub0 = [
     "WARD": 1,
     "DIVISION": 1,
     "TYPE": "A",
-    "OFFICE": "ATTORNEY GENERAL",
+    "OFFICE": "PRESIDENT AND VICE PRESIDENT OF THE UNITED STATES",
     "CANDIDATE": "Write In",
     "PARTY": "",
     "VOTES": 1
@@ -58,6 +58,54 @@ var stub1 = [
   }
 ];
 
+var stub2 = [
+  {
+    "WARD": 1,
+    "DIVISION": 1,
+    "TYPE": "A",
+    "OFFICE": "PRESIDENT AND VICE PRESIDENT OF THE UNITED STATES",
+    "CANDIDATE": "Write In",
+    "PARTY": "",
+    "VOTES": 0
+  },
+  {
+    "WARD": 1,
+    "DIVISION": 1,
+    "TYPE": "A",
+    "OFFICE": "PRESIDENT AND VICE PRESIDENT OF THE UNITED STATES",
+    "CANDIDATE": "Write In",
+    "PARTY": "",
+    "VOTES": 1
+  },
+  {
+    "WARD": 1,
+    "DIVISION": 1,
+    "TYPE": "A",
+    "OFFICE": "PRESIDENT AND VICE PRESIDENT OF THE UNITED STATES",
+    "CANDIDATE": "DARRELL L CASTLE",
+    "PARTY": "CONSTITUTION PARTY",
+    "VOTES": 0
+  },
+  {
+    "WARD": 1,
+    "DIVISION": 1,
+    "TYPE": "A",
+    "OFFICE": "PRESIDENT AND VICE PRESIDENT OF THE UNITED STATES",
+    "CANDIDATE": "DONALD J TRUMP",
+    "PARTY": "REPUBLICAN",
+    "VOTES": 1
+  },
+  {
+    "WARD": 1,
+    "DIVISION": 1,
+    "TYPE": "A",
+    "OFFICE": "PROPOSED CONSTITUTIONAL AMENDMENT",
+    "CANDIDATE": "NO NO",
+    "PARTY": "",
+    "VOTES": 4
+  },
+];
+
 var expected0 = [
   {
     CANDIDATE: "Write In",
@@ -69,13 +117,28 @@ var expected0 = [
   },
 ];
 
-var expected1 = [
+var expected1= [
   {
     CANDIDATE: "Write In",
     VOTES: 0
   },
   {
     CANDIDATE: "JOHN RAFFERTY",
+    VOTES: 1
+  },
+];
+
+var expected2 = [
+  {
+    CANDIDATE: "Write In",
+    VOTES: 1
+  },
+  {
+    CANDIDATE: "DARRELL L CASTLE",
+    VOTES: 0
+  },
+  {
+    CANDIDATE: "DONALD J TRUMP",
     VOTES: 1
   },
 ];
@@ -97,6 +160,14 @@ console.log(
   'collateTotalVotesForCandidates should properly collate candidates and votes when we have unique candidates',
   JSON.stringify(collateTotalVotesForCandidates(stub1)),
   JSON.stringify(expected1)
+);
+
+if (JSON.stringify(collateTotalVotesForCandidates(stub2, 'PRESIDENT AND VICE PRESIDENT OF THE UNITED STATES')) === JSON.stringify(expected2)) console.log('PASSED: ');
+else console.log('FAILED: ');
+console.log(
+  'collateTotalVotesForCandidates should properly collate candidates and votes when we have duplicate candidates and specify an office',
+  JSON.stringify(collateTotalVotesForCandidates(stub2, 'PRESIDENT AND VICE PRESIDENT OF THE UNITED STATES')),
+  JSON.stringify(expected2)
 );
 
 console.log('');

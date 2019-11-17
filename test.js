2b2f41d246aec8014891e233611f37a1698b542a
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-11-17 18:53:21 -0500

    Add tests for election winner function

--- test.js
+++ test.js
@@ -106,6 +106,36 @@ var stub2 = [
   },
 ];
 
+var stub3 = [
+  {
+    CANDIDATE: "Write In",
+    VOTES: 2
+  },
+  {
+    CANDIDATE: "DARRELL L CASTLE",
+    VOTES: 0
+  },
+  {
+    CANDIDATE: "DONALD J TRUMP",
+    VOTES: 1
+  },
+];
+
+var stub4 = [
+  {
+    CANDIDATE: "Write In",
+    VOTES: 2
+  },
+  {
+    CANDIDATE: "DARRELL L CASTLE",
+    VOTES: 2
+  },
+  {
+    CANDIDATE: "DONALD J TRUMP",
+    VOTES: 1
+  },
+];
+
 var expected0 = [
   {
     CANDIDATE: "Write In",
@@ -143,6 +173,24 @@ var expected2 = [
   },
 ];
 
+var expected3 = [
+  {
+    CANDIDATE: "Write In",
+    VOTES: 2
+  },
+];
+
+var expected4 = [
+  {
+    CANDIDATE: "Write In",
+    VOTES: 2
+  },
+  {
+    CANDIDATE: "DARRELL L CASTLE",
+    VOTES: 2
+  },
+];
+
 console.log('=====BEGIN TESTS=====');
 console.log('');
 
@@ -168,6 +216,22 @@ console.log(
   'collateTotalVotesForCandidates should properly collate candidates and votes when we have duplicate candidates and specify an office',
   JSON.stringify(collateTotalVotesForCandidates(stub2, 'PRESIDENT AND VICE PRESIDENT OF THE UNITED STATES')),
   JSON.stringify(expected2)
+);
+
+if (JSON.stringify(findWinners(stub3)) === JSON.stringify(expected3)) console.log('PASSED: ');
+else console.log('FAILED: ');
+console.log(
+  'findWinners should properly determine which candidate in the list has the most votes',
+  JSON.stringify(findWinners(stub3)),
+  JSON.stringify(expected3)
+);
+
+if (JSON.stringify(findWinners(stub4)) === JSON.stringify(expected4)) console.log('PASSED: ');
+else console.log('FAILED: ');
+console.log(
+  'findWinners should properly determine which candidates in the list have the most votes in the event that they have the same final count',
+  JSON.stringify(findWinners(stub4)),
+  JSON.stringify(expected4)
 );
 
 console.log('');
